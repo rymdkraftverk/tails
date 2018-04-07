6cbbdfdc619b02b3145dd0f9f16a9f51d3402df1
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-04-07 12:38:05 +0200

    Add squares in different colors

--- game/src/index.js
+++ game/src/index.js
@@ -2,14 +2,10 @@
 import { Game, Entity, Timer, Key, Debug, Gamepad, Physics, Sound, Net, Text } from 'l1'
 import sprites from './sprites.json'
 
-Game.init(600, 400, sprites, { debug: true, physics: true }).then(() => {
-  Game.getPhysicsEngine().world.gravity.y = 1
-
-  const square = Entity.create('square')
-  const sprite = Entity.addSprite(square, 'square')
-  Entity.addBody(square, Physics.Bodies.rectangle(100, 10, 80, 80))
-  sprite.scale.set(5)
-
-  const floor = Entity.create('floor')
-  Entity.addBody(floor, Physics.Bodies.rectangle(300, 390, 600, 10, { isStatic: true }))
+Game.init(1200, 600, sprites, { debug: true }).then(() => {
+  // const square = Entity.create('square')
+  // const sprite = Entity.addSprite(square, 'square')
+  // sprite.scale.set(5)
+  // sprite.x = 100
+  // sprite.y = 100
 })
