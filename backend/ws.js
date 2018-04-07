ba2db53bdbf96b54e070948045a879771002b115
Lorentz Lasson <lasson.lorentz@gmail.com>
2018-04-07 17:17:30 +0200

    :construction:

--- backend/ws.js
+++ backend/ws.js
@@ -9,11 +9,12 @@ const TYPE = {
 }
 
 const EVENT = {
-  CREATE:    'game.create',
-  CREATED:   'game.created',
-  ANSWER:    'game.join.answer',
-  OFFER:     'game.join.offer',
-  CANDIDATE: 'game.join.controller.candidate',
+  CREATE:               'game.create',
+  CREATED:              'game.created',
+  ANSWER:               'game.join.answer',
+  OFFER:                'game.join.offer',
+  CONTROLLER_CANDIDATE: 'game.join.controller.candidate',
+  GAME_CANDIDATE:       'game.join.game.candidate',
 }
 
 const { log } = console
