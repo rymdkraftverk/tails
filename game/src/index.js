8af345d4bd4a947aab2cbe3f0aec9f0b3280661e
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-04-07 13:12:14 +0200

    Connect to ws server

--- game/src/index.js
+++ game/src/index.js
@@ -1,11 +1,26 @@
 // eslint-disable-next-line no-unused-vars
 import { Game, Entity, Timer, Key, Debug, Gamepad, Physics, Sound, Net, Text } from 'l1'
+import io from 'socket.io-client'
 import sprites from './sprites.json'
 
+const ADDRESS = 'http://localhost:3000'
+const game = {
+  gameCode: '',
+}
+
 Game.init(1200, 600, sprites, { debug: true }).then(() => {
-  // const square = Entity.create('square')
-  // const sprite = Entity.addSprite(square, 'square')
-  // sprite.scale.set(5)
-  // sprite.x = 100
-  // sprite.y = 100
+  const ws = io(ADDRESS)
+  ws.emit('game.create', '')
+  ws.on('game.created', ({ gameCode }) => {
+    console.log('gameId', gameCode)
+    game.gameCode = gameCode
+  })
 })
+
+// function createPlayer(index) {
+//   const square = Entity.create('square-red')
+//   const sprite = Entity.addSprite(square, 'square-red')
+//   sprite.scale.set(5)
+//   sprite.x = 100
+//   sprite.y = 100
+// }
