734db4cf54ab33c9f3e85fd2d9e7188473500e02
Axel Ulmestig <AxelUlmestig@users.noreply.github.com>
2018-04-15 18:37:14 +0200

    Look up game codes in Redis on game creation (#26)
    supply redis info with command line argument --redis e.g.
    $ node index.js --redis http://localhost;6379
    
    if no redis url is supplied we fall back to randomly generating codes
    without collision check

---
+++ backend/make-game-code.js
@@ -0,0 +1,59 @@
+const { URL } = require('url')
+const util = require('util')
+
+const redis = require('redis')
+
+const connectClient = (port, hostname) => {
+  const client = redis.createClient(port, hostname)
+  client.on('error', err => console.log(`Error ${err}`)) // eslint-disable-line no-console
+
+  const set = util.promisify(client.set).bind(client)
+  const exists = util.promisify(client.exists).bind(client)
+  const del = util.promisify(client.del).bind(client)
+
+  return {
+    set,
+    exists,
+    del,
+  }
+}
+
+const makeGameCode = (set, exists) => {
+  const potentialId = Math
+    .random()
+    .toString(36)
+    .substring(2, 6)
+    .toUpperCase()
+
+  return exists(potentialId)
+    .then(doesExist => (doesExist
+      ? makeGameCode()
+      : set(potentialId, potentialId).then(() => potentialId)))
+}
+
+const prepareMakeGameCode = (redisPath) => {
+  if (!redisPath) {
+    return () => new Promise(res => res(Math.random().toString(36).substring(2, 6).toUpperCase()))
+  }
+
+  const url = new URL(redisPath)
+
+  const { set, exists } = connectClient(url.port, url.hostname)
+
+  return () => makeGameCode(set, exists)
+}
+
+const prepareDeleteGameCode = (redisPath) => {
+  if (!redisPath) {
+    return () => new Promise(res => res())
+  }
+
+  console.log('test')
+
+  const url = new URL(redisPath)
+
+  const { del } = connectClient(url.port, url.hostname)
+  return del
+}
+
+module.exports = { prepareMakeGameCode, prepareDeleteGameCode }
