05217b9a201af4606f99d5e68c6939da76c0f5d6
Simon Lagos <simon.lagos@jayway.com>
2018-05-06 17:53:27 +0200

    Enable restarting the game from lobby

--- common/events.js
+++ common/events.js
@@ -4,6 +4,7 @@ const EVENTS = {
   CREATED:   'game.created',
   GAME_START: 'game.start',
   GAME_STARTED: 'game.started',
+  GAME_OVER: 'game.over',
   ANSWER:    'game.join.answer',
   OFFER:     'game.join.offer',
   CONTROLLER_CANDIDATE: 'game.join.controller.candidate',
--- game/src/gameover.js
+++ game/src/gameover.js
@@ -1,4 +1,5 @@
 import { Entity, Timer } from 'l1'
+import EVENTS from '../../common/events'
 import { createLobby, players } from './lobby'
 import { game } from '.'
 import { big } from './util/text'
@@ -17,6 +18,11 @@ const pause = () => ({
   timer: Timer.create(100),
   run:   (b) => {
     if (b.timer.run()) {
+      Object
+        .values(game.controllers)
+        .forEach(({ channel }) =>
+          channel.send(JSON.stringify({ event: EVENTS.GAME_OVER, payload: {} })))
+
       createLobby(game.gameCode, Object.values(players))
     }
   },
