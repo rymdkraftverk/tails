868ea8c34293df8339d980f24224fc9031e17367
rbarlin <robert.barlin@gmail.com>
2018-05-10 12:09:56 +0200

    disable select text in controller lobby

--- controller/src/GameLobby.js
+++ controller/src/GameLobby.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 
+const style = {
+  '-webkit-touch-callout': 'none',
+  '-webkit-user-select':   'none',
+  '-khtml-user-select':    'none',
+  '-moz-user-select':      'none',
+  '-ms-user-select':       'none',
+  'user-select':           'none',
+}
+
 /* eslint-disable-next-line fp/no-class */
 class GameLobby extends Component {
   render() {
     return (
       <div id="game-lobby-container" style={{ backgroundColor: this.props.playerColor }}>
-        <div className="flex-box" onClick={this.props.startGame}>Start Game!</div>
+        <div style={style} className="flex-box" onClick={this.props.startGame}>Start Game!</div>
       </div>
     )
   }
