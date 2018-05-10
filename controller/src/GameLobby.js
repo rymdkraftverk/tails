736d9964e2ea6ba9796f421aeb161ec284648533
rbarlin <robert.barlin@gmail.com>
2018-05-10 12:44:00 +0200

    align player colors in controller

--- controller/src/GameLobby.js
+++ controller/src/GameLobby.js
@@ -9,11 +9,29 @@ const style = {
   'user-select':           'none',
 }
 
+const playerBackgroundColors = {
+  blue:      '#3E5AFF',
+  brown:     '#945200',
+  green:     '#95A783',
+  orange:    '#FF9201',
+  pink:      '#FF85FF',
+  purple:    '#A73D8D',
+  red:       '#CB725D',
+  turqouise: '#009B9D',
+  white:     '#EEEDEF',
+  yellow:    '#ECF257',
+}
+
+const playerColorToBackgroundColor = (player) => {
+  const background = playerBackgroundColors[player]
+  return background || 'white'
+}
+
 /* eslint-disable-next-line fp/no-class */
 class GameLobby extends Component {
   render() {
     return (
-      <div id="game-lobby-container" style={{ backgroundColor: this.props.playerColor }}>
+      <div id="game-lobby-container" style={{ backgroundColor: playerColorToBackgroundColor(this.props.playerColor) }}>
         <div style={style} className="flex-box" onClick={this.props.startGame}>Start Game!</div>
       </div>
     )
