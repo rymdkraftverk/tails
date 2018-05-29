c260f1cef3752d45b2bbcab19d967e619201dce6
Simon Lagos <simon.lagos@jayway.com>
2018-05-29 21:09:41 +0200

    Log errors in controller

--- controller/src/App.js
+++ controller/src/App.js
@@ -32,7 +32,7 @@ const APP_STATE = {
   GAME_PLAYING:    'game-playing',
 }
 
-const { warn } = console
+const { warn, error } = console
 
 const isMobileDevice = () => (typeof window.orientation !== 'undefined') || (navigator.userAgent.indexOf('IEMobile') !== -1)
 const isSafari = () => navigator.userAgent.indexOf('Safari') > -1
@@ -101,6 +101,11 @@ class App extends Component {
       state.error = true
       connectionCleanUp({ ws, peer, channel })
       this.setState({ appState: APP_STATE.LOCKER_ROOM, channel: null, error: true })
+    }
+
+    const onError = (event) => {
+      error(event)
+      cleanUp()
     }
 
     ws.onopen = () => {
@@ -180,7 +185,7 @@ class App extends Component {
       }
     }
 
-    channel.onerror = cleanUp
+    channel.onerror = onError
     channel.onclose = cleanUp
   }
 
