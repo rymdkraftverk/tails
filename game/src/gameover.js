58a3c36a2482b43d731554d151dcb7d8d05c9e1d
Simon Lagos <simon.lagos@jayway.com>
2018-06-13 07:50:20 +0200

    New version of winner text animation

--- game/src/gameover.js
+++ game/src/gameover.js
@@ -1,11 +1,10 @@
 import { Entity, Timer } from 'l1'
-import { createEaseOut, createEaseIn } from './magic'
+import { createEaseOut, createEaseIn, createEaseInAndOut } from './magic'
 import EVENTS from '../../common/events'
 import { createLobby, players } from './lobby'
-import { game } from '.'
+import { game, WIDTH } from '.'
 import { big } from './util/textStyles'
 import { connSend } from './conn'
-
 
 const TIME_UNTIL_GAME_RESTARTS = 240
 
@@ -13,8 +12,8 @@ export function transitionToGameover() {
   const gameover = Entity.create('game-over')
   const { winner } = game.lastResult
   const text = Entity.addText(gameover, `Winner is ${winner}!`, { ...big, fill: winner }, { zIndex: 100 })
-  text.position.x = 0
-  text.position.y = 200
+  text.position.set(-300, 200)
+  text.anchor.set(0.5)
   gameover.behaviors.winnerTextAnimation = winnerTextAnimation()
 
   gameover.behaviors.pause = pause()
@@ -34,34 +33,12 @@ const pause = () => ({
   },
 })
 
-const winnerTextAnimationStates = {
-  ENTERING: 'ENTERING',
-  HOLDING:  'HOLDING',
-  EXITING:  'EXITING',
-}
-
-const ENTERING_END_X = 300
-const HOLDING_END_X = 500
-const EXITING_END_X = 1100
-
 const winnerTextAnimation = () => ({
-  entrance: createEaseOut(ENTERING_END_X, 4),
-  leaving:  createEaseIn(EXITING_END_X, 100000),
-  state:    winnerTextAnimationStates.ENTERING,
-  run:      (b, e) => {
-    if (b.state === winnerTextAnimationStates.ENTERING) {
-      if (e.text.position.x >= ENTERING_END_X) {
-        b.state = winnerTextAnimationStates.HOLDING
-      }
-      e.text.position.x += b.entrance(e.text.position.x)
-    } else if (b.state === winnerTextAnimationStates.HOLDING) {
-      if (e.text.position.x >= HOLDING_END_X) {
-        b.state = winnerTextAnimationStates.EXITING
-      }
-      e.text.position.x += 4
-    } else if (b.state === winnerTextAnimationStates.EXITING) {
-      e.text.position.x += b.leaving(e.text.position.x)
-    }
+  animation: createEaseInAndOut(120, 0.15, WIDTH / 2),
+  // animation: createEaseInAndOut(0, 1200, 0, 240),
+  tick:      0,
+  run:       (b, e) => {
+    e.text.position.x = b.animation(b.tick)
+    b.tick += 1
   },
 })
-
--- game/src/magic.js
+++ game/src/magic.js
@@ -17,10 +17,21 @@ export const createParabolaAngle = (x1, x2, modifier = 1) => (x) => {
   return Math.atan((Math.abs(x1 - x2) * modifier) * normalizer * ((2 * x) - x1 - x2))
 }
 
+/**
+ *
+ * @param {*} endX
+ * @param {*} minSpeed
+ */
 export const createEaseOut = (endX, minSpeed = 0.5) => x => easeOut(endX, x, minSpeed)
 
 const easeOut = (endX, x, minSpeed) => Math.max(Math.abs((endX - x)) * 0.1, minSpeed)
 
+/**
+ *
+ * @param {*} endX
+ * @param {*} modifier
+ * @param {*} maxSpeed
+ */
 export const createEaseIn =
   (endX, modifier = 1, maxSpeed = 10) => x => easeIn(endX, x, maxSpeed, modifier)
 
@@ -28,6 +39,23 @@ const easeIn = (endX, x, maxSpeed, modifier) => Math.min(modifier / Math.abs((en
 
 // export const createEaseInOut = (startX, endX, minSpeed = 0.5) => (x) => Math.max(Math.abs((endX - x)) * 0.1, minSpeed);
 
+export const createEaseInAndOut = (offset, factor, middle) => t =>
+  middle + ((factor * (t - offset)) ** 3)
+
+// export const createEaseInAndOut = (x1, x2, t1, t2) => {
+//   const speed = ((2 * (2 / 3)) * (((3 * x1) - x2) ** (1 / 3))) / ((3 * t1) - t2)
+//   const timeOffset = (t2 - t1) / 2
+//   const positionOffset = (x2 - x1) / 2
+
+//   return t => ((speed * (t - timeOffset)) ** 3) + positionOffset
+// }
+
+/**
+ *
+ * @param {*} start
+ * @param {*} end
+ * @param {*} speed
+ */
 export const createSine = (start, end, speed) => (now) => {
   const middle = ((start + end) / 2)
   return middle + ((middle - start) * Math.sin((now * Math.PI * 2) / speed))
