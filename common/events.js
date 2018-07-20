e2e5038e628007cf3ea8ae2f7fb3b96ecf0c8bc4
Robert Bärlin <robert.barlin@jayway.com>
2018-07-20 10:03:08 +0200

    Align spacing in events for RTC

--- common/events.js
+++ common/events.js
@@ -9,9 +9,9 @@ const EVENTS = {
     NOT_FOUND:           'receiver.join.initiator.notfound',
   },
   RTC: {
-    ROUND_END:        'round.end',
-    ROUND_START:      'round.start',
-    ROUND_STARTED:    'round.started',
+    ROUND_END:       'round.end',
+    ROUND_START:     'round.start',
+    ROUND_STARTED:   'round.started',
     PLAYER_JOINED:   'player.joined',
     PLAYER_MOVEMENT: 'player.movement',
   }
