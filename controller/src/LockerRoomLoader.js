03f91ec6fd964eda12ee06f34fd58b0dca633514
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-07-22 11:59:47 +0200

    Fix indentation in LockerRoomLoader

--- controller/src/LockerRoomLoader.js
+++ controller/src/LockerRoomLoader.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function LockerRoomLoader() {
   return (
-      <div style={{ touchAction: 'manipulation' }}>
-        <div id="loader" style={{ touchAction: 'manipulation' }}></div>
-      </div>
+    <div style={{ touchAction: 'manipulation' }}>
+      <div id="loader" style={{ touchAction: 'manipulation' }}></div>
+    </div>
   )
 }
 
