c70aa0407efa7ccefb4fc7baf317bed9428f6370
Robert Bärlin <robert.barlin@jayway.com>
2018-07-22 14:03:32 +0200

    Style App component

--- controller/src/index.js
+++ controller/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
 import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const AppStyled = styled(App)`
+  touch-action: manipulation;
+`
+
+ReactDOM.render(<AppStyled />, document.getElementById('root'))
 registerServiceWorker()
