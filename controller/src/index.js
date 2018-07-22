b20c7216362bd2be3387f8bf35cb20e25188a396
Robert Bärlin <robert.barlin@jayway.com>
2018-07-22 15:34:03 +0200

    Delete index.css

--- controller/src/index.js
+++ controller/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import styled, { injectGlobal } from 'styled-components'
-import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 
