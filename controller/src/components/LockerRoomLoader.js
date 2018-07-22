c833c48ba60e18373c006934d2be55ad1cc43536
Robert Bärlin <robert.barlin@jayway.com>
2018-07-22 13:45:43 +0200

    Replace LockerRoomLoader css with styled components

--- controller/src/components/LockerRoomLoader.js
+++ controller/src/components/LockerRoomLoader.js
@@ -1,12 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  touch-action: manipulation;
+`
+const Spinner = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  z-index: 1;
+  width: 150px;
+  height: 150px;
+  margin: -75px 0 0 -75px;
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #3498db;
+  width: 120px;
+  height: 120px;
+  -webkit-animation: spin 2s linear infinite;
+  animation: spin 2s linear infinite;
+  touch-action: manipulation;
+`
 
 const LockerRoomLoader = () => (
-  <div style={{ touchAction: 'manipulation' }}>
-    <div
-      id="loader"
-      style={{ touchAction: 'manipulation' }}
-    />
-  </div>
+  <Container>
+    <Spinner />
+  </Container>
 )
 
 export default LockerRoomLoader
