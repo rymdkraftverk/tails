642b4a4e3f092f0986c9458ae5a456f45c905252
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-07-22 11:56:10 +0200

    Remove redundant code in LockerRoomLoader

--- controller/src/LockerRoomLoader.js
+++ controller/src/LockerRoomLoader.js
@@ -1,23 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class LockerRoomLoader extends Component {
-  render() {
-    const {
-      error,
-    } = this.props
-
-    return (
+function LockerRoomLoader() {
+  return (
       <div style={{ touchAction: 'manipulation' }}>
-        {!error ? <div id="loader" style={{ touchAction: 'manipulation' }}></div> : null}
-        {error ?
-          <button
-            id="lobby-join-button"
-            style={{ touchAction: 'manipulation' }}
-            onClick={this.joinGame}>Join</button>
-          : null}
+        <div id="loader" style={{ touchAction: 'manipulation' }}></div>
       </div>
-    )
-  }
+  )
 }
 
 export default LockerRoomLoader
