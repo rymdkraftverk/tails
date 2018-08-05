f8c1f6dd631797cf934fc66cd8670c56cb2c4e9c
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-08-05 13:10:11 +0200

    Move TODO comment to level1 project board

--- game/src/bounce.js
+++ game/src/bounce.js
@@ -14,7 +14,6 @@ export default () => ({
     b.tick += 1
     e.asset.scale.set(-1 * b.animation(b.tick))
     if (b.tick >= 20) {
-      // TODO: The behavior should not have to know how it is called on the entity
       // eslint-disable-next-line fp/no-delete
       delete e.behaviors.bounce
     }
