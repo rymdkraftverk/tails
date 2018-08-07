48ebb0097d4ede27955b851082be66c556308bb2
axel.ulmestig@gmail.com <axel.ulmestig@gmail.com>
2018-08-07 20:26:26 +0200

    Vibrate controller on player death

--- controller/src/components/PlayerDead.js
+++ controller/src/components/PlayerDead.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
 
+const noop = () => {}
+
+navigator.vibrate = (navigator.vibrate ||
+  navigator.webkitVibrate ||
+  navigator.mozVibrate ||
+  navigator.msVibrate || noop)
+
 export default class PlayerDead extends Component {
     render() {
+        navigator.vibrate(100)
         return (<b>You dead</b>)
     }
 }
