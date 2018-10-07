acb3511b9db985f66e475b655adf024863fbbc4f
Lorentz Lasson <lasson.lorentz@gmail.com>
2018-10-07 19:38:36 +0200

    Use AAAA as code in alias

--- alias.sh
+++ alias.sh
@@ -1,2 +1,2 @@
 # docker compose locally
-alias rkv_dcl="REACT_APP_WS_ADDRESS=ws://localhost:3000 docker-compose up"
+alias rkv_dcl="REACT_APP_WS_ADDRESS=ws://localhost:3000 GAME_CODE=AAAA docker-compose up"
