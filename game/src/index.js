5ce3ae06737aefa524f41a35b674f1252ef7ff8c
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-10-17 12:33:38 +0200

    Move the amount of behaviors overlay to a debug folder

--- game/src/index.js
+++ game/src/index.js
@@ -272,17 +272,6 @@ resizeGame()
 
 window.addEventListener('resize', resizeGame)
 
-const div = document.createElement('div')
-div.style.color = 'red'
-div.style.position = 'absolute'
-div.style.top = '0px'
-div.style.zIndex = 10
-document.body.appendChild(div)
-
-setInterval(() => {
-  div.innerHTML = `b: ${l1.getAllBehaviors().length}`
-}, 1000)
-
 const printBehaviors = () => {
   console.log('BEHAVIORS:')
   l1.getAllBehaviors()
