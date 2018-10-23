18f5b99706825674e2cc79d104d6b618f4af820f
Lorentz <lorentzlasson@users.noreply.github.com>
2018-10-23 09:27:23 +0200

    Transmit steering commands over reliable channel (#481)

--- controller/src/channelConfigs.js
+++ controller/src/channelConfigs.js
@@ -4,11 +4,9 @@ import protobufDescriptor from './protobuf/bundle.json'
 // configuration explanation:
 // https://jameshfisher.com/2017/01/17/webrtc-datachannel-reliability.html
 export default [{
-  // "udpLike"
-  name:   Channel.UNRELIABLE,
+  name:   Channel.RELIABLE_STEERING,
   config: {
-    ordered:        false,
-    maxRetransmits: 0,
+    ordered: true,
   },
   protobuf: {
     descriptor: protobufDescriptor,
