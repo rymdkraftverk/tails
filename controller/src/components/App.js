4c28a1dd6f7c9f5aae4a22efc5896aacb9c56b25
Lorentz <lorentzlasson@users.noreply.github.com>
2018-10-29 21:19:18 +0100

    Display friendly message for "awaiting next turn" (#518)

--- controller/src/components/App.js
+++ controller/src/components/App.js
@@ -8,6 +8,7 @@ import LockerRoom from './LockerRoom'
 import LockerRoomLoader from './LockerRoomLoader'
 import GameLobby from './GameLobby'
 import GamePlaying from './GamePlaying'
+import AwatingNextRound from './AwatingNextRound'
 import PlayerDead from './PlayerDead'
 import isMobileDevice from '../util/isMobileDevice'
 import { getLastGameCode, setLastGameCode } from '../util/localStorage'
@@ -19,7 +20,7 @@ const TIMEOUT_SECONDS = 20
 
 log('REACT_APP_WS_ADDRESS', WS_ADDRESS)
 
-const APP_STATE = {
+const AppState = {
   LOCKER_ROOM:     'locker-room',
   GAME_CONNECTING: 'game-connecting',
   GAME_LOBBY:      'game-lobby',
@@ -27,9 +28,37 @@ const APP_STATE = {
   PLAYER_DEAD:     'player-dead',
 }
 
+const colorState = ({ started, color }) => (
+  {
+    appState: started
+      ? AppState.AWAITING_NEXT_ROUND
+      : AppState.GAME_LOBBY,
+    playerColor: color,
+  }
+)
+
+const errorState = message => ({
+  appState: AppState.LOCKER_ROOM,
+  error:    message,
+})
+
+const eventState = ({ event, payload }) => {
+  switch (event) {
+    case Event.A_PLAYER_JOINED: return payload
+    case Event.A_PLAYER_LEFT: return payload
+    case Event.CONTROLLER_COLOR: return colorState(payload)
+    case Event.GAME_FULL: return errorState('Game is full')
+    case Event.PLAYER_DIED: return { appState: AppState.PLAYER_DEAD }
+    case Event.ROUND_END: return { appState: AppState.GAME_LOBBY }
+    case Event.ROUND_START: return { appState: AppState.GAME_PLAYING }
+    case Event.ROUND_STARTED: return { appState: AppState.GAME_PLAYING }
+    default: return null
+  }
+}
+
 class App extends Component {
   state = {
-    appState:    APP_STATE.LOCKER_ROOM,
+    appState:    AppState.LOCKER_ROOM,
     fullscreen:  false,
     gameCode:    '',
     playerColor: null,
@@ -43,53 +72,27 @@ class App extends Component {
     this.setState({ gameCode })
   }
 
-  onData = ({ event, payload }) => {
-    if (event === Event.CONTROLLER_COLOR) {
-      if (!payload.started) {
-        this.setState({
-          appState:    APP_STATE.GAME_LOBBY,
-          playerColor: payload.color,
-        })
-      } else {
-        this.setState({
-          appState: APP_STATE.PLAYER_DEAD,
-        })
-      }
-    } else if (event === Event.ROUND_START) {
-      this.setState({
-        appState: APP_STATE.GAME_PLAYING,
-      })
-    } else if (event === Event.GAME_FULL) {
-      this.displayError('Game is full')
-    } else if (event === Event.ROUND_STARTED) {
-      this.setState({
-        appState: APP_STATE.GAME_PLAYING,
-      })
-    } else if (event === Event.ROUND_END) {
-      this.setState({
-        appState: APP_STATE.GAME_LOBBY,
-      })
-    } else if (event === Event.A_PLAYER_JOINED) {
-      this.setState(payload)
-    } else if (event === Event.A_PLAYER_LEFT) {
-      this.setState(payload)
-    } else if (event === Event.PLAYER_DIED) {
-      this.setState({
-        appState: APP_STATE.PLAYER_DEAD,
-      })
+  onData = (message) => {
+    const state = eventState(message)
+
+    if (!state) {
+      logError(`Unexpected event in message: ${message}`)
+      return
     }
+
+    this.setState(state)
   }
 
   onJoin = () => {
     const { gameCode } = this.state
-    this.setState({ appState: APP_STATE.GAME_CONNECTING, error: '', fullscreen: true })
+    this.setState({ appState: AppState.GAME_CONNECTING, error: '', fullscreen: true })
     setLastGameCode(gameCode)
     setTimeout(this.checkConnectionTimeout, TIMEOUT_SECONDS * 1000)
     this.connectToGame(gameCode)
   };
 
   displayError = (message) => {
-    this.setState({ appState: APP_STATE.LOCKER_ROOM, error: message })
+    this.setState(errorState(message))
   }
 
   alertIfNoRtc = () => {
@@ -111,7 +114,7 @@ class App extends Component {
     })
 
   checkConnectionTimeout = () => {
-    if (this.state.appState === APP_STATE.GAME_CONNECTING) {
+    if (this.state.appState === AppState.GAME_CONNECTING) {
       this.displayError('Failed to connect, try again!')
     }
   };
@@ -151,7 +154,7 @@ class App extends Component {
 
   startGame = () => {
     this.sendReliable({ event: Event.ROUND_START })
-    this.setState({ appState: APP_STATE.GAME_PLAYING })
+    this.setState({ appState: AppState.GAME_PLAYING })
   }
 
   enableFullscreen = () => this.state.fullscreen && isMobileDevice()
@@ -176,7 +179,7 @@ class App extends Component {
         onChange={fullscreen => this.setState({ fullscreen })}
       >
         {
-          appState === APP_STATE.LOCKER_ROOM
+          appState === AppState.LOCKER_ROOM
             ?
               <LockerRoom
                 clearError={this.clearError}
@@ -188,12 +191,12 @@ class App extends Component {
             : null
         }
         {
-          appState === APP_STATE.GAME_CONNECTING
+          appState === AppState.GAME_CONNECTING
             ? <LockerRoomLoader />
             : null
         }
         {
-          appState === APP_STATE.GAME_LOBBY
+          appState === AppState.GAME_LOBBY
             ?
               <GameLobby
                 startGame={this.startGame}
@@ -203,7 +206,7 @@ class App extends Component {
             : null
         }
         {
-          appState === APP_STATE.GAME_PLAYING
+          appState === AppState.GAME_PLAYING
             ?
               <GamePlaying
                 send={this.sendSteering}
@@ -212,9 +215,17 @@ class App extends Component {
             : null
         }
         {
-          appState === APP_STATE.PLAYER_DEAD
+          appState === AppState.PLAYER_DEAD
             ?
               <PlayerDead />
+            : null
+        }
+        {
+          appState === AppState.AWAITING_NEXT_ROUND
+            ?
+              <AwatingNextRound
+                playerColor={Color[playerColor]}
+              />
             : null
         }
       </Fullscreen>
---
+++ controller/src/components/AwatingNextRound.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import * as R from 'ramda'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import IOSDisableDoubleTap from './IOSDisableDoubleTap'
+
+const Container = styled(IOSDisableDoubleTap)`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  user-select: none;
+  background: ${R.prop('color')};
+`
+
+const Text = styled.b`
+  height: 30vh;
+  font-family: inherit;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const AwatingNextRound = ({ playerColor }) => (
+  <Container
+    color={playerColor}
+  >
+    <Text>Awaiting next round</Text>
+  </Container>
+)
+
+AwatingNextRound.propTypes = {
+  playerColor: PropTypes.string.isRequired,
+}
+
+export default AwatingNextRound
