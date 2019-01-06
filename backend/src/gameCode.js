14559376e73386508300fe4a79a39802cd668ccf
Lorentz Lasson <lasson.lorentz@gmail.com>
2019-01-06 13:41:08 +0100

    Use complete URL instead of host and port
    Supports connection string provided by heroku

--- backend/src/gameCode.js
+++ backend/src/gameCode.js
@@ -57,9 +57,9 @@ const createUniqueRandomCode = (set, exists, attempts) => {
 }
 
 const createRedisInterface = () => {
-  const { port, hostname } = new URL(process.env.REDIS_PATH)
-
-  const client = redis.createClient(port, hostname)
+  const client = redis.createClient({
+    url: process.env.REDIS_PATH,
+  })
   client.on('error', logRedisError)
 
   const p = promisify(client)
