c91a22bb2ac47ac91395c4b79859b43f04341151
Simon Lagos <simon.lagos.sallhed@gmail.com>
2019-01-13 13:23:42 +0100

    Display game title in controller

---
+++ controller/src/components/Logo.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components'
+import LogoImg from '../images/logo.png'
+
+const Container = styled.div`
+  display: flex;
+  margin-top: 32px;
+`;
+
+const Img = styled.img`
+  height: 20vw;
+  width: auto;
+`
+
+const Title = styled.p`
+  color: white;
+  font-size: 5vw;
+  margin-top: 3vw;
+`;
+
+const Logo = () => {
+  return (
+    <Container>
+      <Img src={LogoImg} alt="Logo"/>
+      <Title>tails</Title>
+    </Container>
+  )
+}
+
+export default Logo
