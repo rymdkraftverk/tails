aa24fa6bcfdc3b43726a190481d889662db0adfd
Simon Lagos <simon.lagos.sallhed@gmail.com>
2019-02-10 13:06:07 +0100

    Update message

--- controller/src/components/App.js
+++ controller/src/components/App.js
@@ -135,7 +135,7 @@ class App extends Component {
 
   checkConnectionTimeout = () => {
     if (this.state.appState === AppState.GAME_CONNECTING) {
-      this.displayError('Connection failed, joining Wi-Fi can help')
+      this.displayError('Connection failed, joining Wi-Fi may help')
     }
   };
 
