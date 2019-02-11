9de474c03e9fa8d13d657355949de3dd7d86d660
Lorentz Lasson <lasson.lorentz@gmail.com>
2019-02-11 11:38:31 +0100

    Ease local hosting

---
+++ localIp.js
@@ -0,0 +1,19 @@
+const os = require('os')
+const ifaces = os.networkInterfaces()
+const R = require('ramda')
+
+const getLocalIp = R.pipe(
+  R.toPairs,
+  R.chain(R.last),
+  R.find(
+    R.and(
+      R.propEq('family', 'IPv4'),
+      R.propEq('internal', false)
+    )
+  ),
+  R.prop('address')
+)
+
+console.log(
+  getLocalIp(ifaces)
+)
---
+++ run.sh
@@ -0,0 +1,6 @@
+. .env
+docker-compose up backend game controller
+
+# To run locally with redis:
+# 1. Add "redis" to list of services above
+# 2. Uncomment REDIS_URL in .env
