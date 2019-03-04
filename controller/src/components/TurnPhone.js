7ecfe8bbf45055fbb09b84c6c72eeee36c927d64
Simon Lagos <simon.lagos.sallhed@gmail.com>
2019-03-04 12:58:49 +0100

    Show a message to "turn the phone" when in portrait mode

---
+++ controller/src/components/TurnPhone.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+import Logo from './Logo'
+
+const Content = styled.div`
+  color: white;
+  text-align: center;
+  margin-bottom: auto;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: center;
+`
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+
+const TurnPhone = props => {
+  return (
+    <PageContainer>
+      <Logo />
+      <Content>Please turn your phone to landscape</Content>
+    </PageContainer>
+  )
+}
+
+TurnPhone.propTypes = {
+
+}
+
+export default TurnPhone
