8a051e9a99842210ba27a1c7b1f608649c5d941e
Simon Lagos <simon.lagos.sallhed@gmail.com>
2019-03-11 10:43:49 +0100

    Add an animation to the "turn phone" instructions (#742)

--- controller/src/components/TurnPhone.js
+++ controller/src/components/TurnPhone.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import Logo from './Logo'
+import turnPhone from '../images/turnPhone.gif'
 
 const Content = styled.div`
   color: white;
@@ -10,6 +11,7 @@ const Content = styled.div`
   height: 100%;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
 `
 
 const PageContainer = styled.div`
@@ -18,12 +20,23 @@ const PageContainer = styled.div`
   height: 100vh;
 `;
 
+const GIF = styled.img`
+  width: 100vw;
+  height: auto;
+  margin-bottom: -80px;
+  margin-top: -200px;
+`;
 
 const TurnPhone = props => {
   return (
     <PageContainer>
       <Logo />
-      <Content>Please turn your phone to landscape</Content>
+      <Content>
+        <GIF src={turnPhone} />
+        <div>
+          Please turn your phone to landscape
+        </div>
+      </Content>
     </PageContainer>
   )
 }
