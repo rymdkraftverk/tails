655120d6e570250aaba5f58746c890f6fde00577
Lorentz Lasson <lorentzlasson@users.noreply.github.com>
2019-03-24 17:38:39 +0100

    Store code in session storage (#782)
    Expires with tab/window close

--- controller/src/util/sessionStorage.js
+++ controller/src/util/sessionStorage.js
@@ -1,9 +1,9 @@
 export const getLastGameCode = () => {
-  const gameCode = localStorage.getItem('gameCode')
+  const gameCode = sessionStorage.getItem('gameCode')
   return gameCode || ''
 }
 
 export const setLastGameCode = gameCode => {
-  localStorage.setItem('gameCode', gameCode)
+  sessionStorage.setItem('gameCode', gameCode)
   return gameCode
 }
