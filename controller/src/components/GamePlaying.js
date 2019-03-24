d0fcddba73d411b3c3f7300e4cbe5539dcd4184a
Lorentz Lasson <lorentzlasson@users.noreply.github.com>
2019-03-24 21:08:30 +0100

    Hide tap-interface when in gyro mode (#785)
    * 🔨 Extract tap steering component
    
    * Display gyro view when toggled

--- controller/src/components/GamePlaying.js
+++ controller/src/components/GamePlaying.js
@@ -1,30 +1,16 @@
 import * as R from 'ramda'
-import React, { Component } from 'react'
+import React from 'react'
 import Switch from 'react-switch'
 import PropTypes from 'prop-types'
-import { Event, SteeringCommand } from 'common'
 import styled from 'styled-components/macro'
 import Div100vh from 'react-div-100vh'
 import IOSDisableDoubleTap from './IOSDisableDoubleTap'
 import ScrollLock from './ScrollLock'
-
-const SteerButton = styled.div`
-  flex: 1;
-  user-select: none;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  font-size: 100pt;
-  background: ${R.prop('playerColor')};
-`
+import TapSteering from './TapSteering'
+import GyroSteering from './GyroSteering'
 
 const VerticalSeparator = styled.div`
   width: 20px;
-`
-
-const BlackSeparator = styled(VerticalSeparator)`
-  background: black;
 `
 
 const TogglePane = styled.div`
@@ -42,12 +28,7 @@ const TogglePane = styled.div`
 const Container = styled(Div100vh)`
   display: flex;
   flex-direction: column;
-`
-
-const SteeringContainer = styled.div`
-  display: flex;
-  overflow: hidden;
-  height: 100%;
+  background: ${R.prop('playerColor')};
 `
 
 const noop = () => {}
@@ -59,55 +40,22 @@ navigator.vibrate =
   navigator.msVibrate ||
   noop
 
-class GamePlaying extends Component {
-  state = {
-    lastOrientation: {},
-  }
+const GamePlaying = props => {
+  const { gyro, playerColor, send, setGyro } = props
 
-  sendCommand = command => () => {
-    this.props.send({
-      event: Event.PLAYER_MOVEMENT,
-      payload: command,
-    })
-  }
-
-  render() {
-    const { gyro, playerColor, setGyro } = this.props
-
-    return (
-      <IOSDisableDoubleTap>
-        <ScrollLock />
-        <Container>
-          <TogglePane>
-            GYRO
-            <VerticalSeparator />
-            <Switch onChange={setGyro} checked={gyro} />
-          </TogglePane>
-          <SteeringContainer>
-            <SteerButton
-              playerColor={playerColor}
-              onMouseDown={this.sendCommand(SteeringCommand.LEFT)}
-              onMouseUp={this.sendCommand(SteeringCommand.NONE)}
-              onTouchStart={this.sendCommand(SteeringCommand.LEFT)}
-              onTouchEnd={this.sendCommand(SteeringCommand.NONE)}
-            >
-              <div>{'<'} </div>
-            </SteerButton>
-            <BlackSeparator />
-            <SteerButton
-              playerColor={playerColor}
-              onMouseDown={this.sendCommand(SteeringCommand.RIGHT)}
-              onMouseUp={this.sendCommand(SteeringCommand.NONE)}
-              onTouchStart={this.sendCommand(SteeringCommand.RIGHT)}
-              onTouchEnd={this.sendCommand(SteeringCommand.NONE)}
-            >
-              <div>{'>'} </div>
-            </SteerButton>
-          </SteeringContainer>
-        </Container>
-      </IOSDisableDoubleTap>
-    )
-  }
+  return (
+    <IOSDisableDoubleTap>
+      <ScrollLock />
+      <Container playerColor={playerColor}>
+        <TogglePane>
+          GYRO
+          <VerticalSeparator />
+          <Switch onChange={setGyro} checked={gyro} />
+        </TogglePane>
+        {gyro ? <GyroSteering /> : <TapSteering send={send} />}
+      </Container>
+    </IOSDisableDoubleTap>
+  )
 }
 
 GamePlaying.propTypes = {
---
+++ controller/src/components/TapSteering.js
@@ -0,0 +1,62 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import styled from 'styled-components/macro'
+import { Event, SteeringCommand } from 'common'
+
+const Separator = styled.div`
+  background: black;
+  width: 20px;
+`
+
+const Container = styled.div`
+  display: flex;
+  overflow: hidden;
+  height: 100%;
+`
+
+const Button = styled.div`
+  flex: 1;
+  user-select: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 100pt;
+`
+
+const TapSteering = props => {
+  const sendCommand = command => () => {
+    props.send({
+      event: Event.PLAYER_MOVEMENT,
+      payload: command,
+    })
+  }
+
+  return (
+    <Container>
+      <Button
+        onMouseDown={sendCommand(SteeringCommand.LEFT)}
+        onMouseUp={sendCommand(SteeringCommand.NONE)}
+        onTouchStart={sendCommand(SteeringCommand.LEFT)}
+        onTouchEnd={sendCommand(SteeringCommand.NONE)}
+      >
+        <div>{'<'} </div>
+      </Button>
+      <Separator />
+      <Button
+        onMouseDown={sendCommand(SteeringCommand.RIGHT)}
+        onMouseUp={sendCommand(SteeringCommand.NONE)}
+        onTouchStart={sendCommand(SteeringCommand.RIGHT)}
+        onTouchEnd={sendCommand(SteeringCommand.NONE)}
+      >
+        <div>{'>'} </div>
+      </Button>
+    </Container>
+  )
+}
+
+TapSteering.propTypes = {
+  send: PropTypes.func.isRequired,
+}
+
+export default TapSteering
