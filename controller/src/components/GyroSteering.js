0ddd8c2ae25bc64fa63efc023806bc7f54efe8d9
Lorentz Lasson <lasson.lorentz@gmail.com>
2019-05-04 19:16:30 +0200

    Draw gyro line

--- controller/src/components/GyroSteering.js
+++ controller/src/components/GyroSteering.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components/macro'
 
-const Container = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
+const Canvas = styled.canvas`
   height: 100%;
-  justify-content: center;
-  font-size: 5vw;
 `
 
 const GyroSteering = ({ angle }) => {
-  return <Container>Tilt phone to steer! {Math.round(angle)}</Container>
+  const canvasRef = useRef(null)
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const ctx = canvas.getContext('2d');
+
+    // Reset old line
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    // Static setup
+    ctx.beginPath()
+    ctx.strokeStyle = 'black'
+    ctx.lineWidth = 10
+
+    // Calculate line
+    const diff = canvas.height / (18 * 2) * angle
+    const middleY = canvas.height / 2
+    const y1 = middleY + diff
+    const y2 = middleY - diff
+
+    // Draw line
+    ctx.moveTo(0, y1)
+    ctx.lineTo(canvas.width, y2)
+    ctx.stroke()
+
+  }, [angle])
+
+  return <Canvas ref={canvasRef} />
 }
 
 GyroSteering.propTypes = {
