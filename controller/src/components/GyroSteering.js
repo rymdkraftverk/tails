e38b7dd4737e909427017f8567955059df321b79
Lorentz Lasson <lasson.lorentz@gmail.com>
2019-05-04 20:03:22 +0200

    Store ctx and dims in state

--- controller/src/components/GyroSteering.js
+++ controller/src/components/GyroSteering.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components/macro'
 
@@ -9,27 +9,38 @@ const Canvas = styled.canvas`
 const GyroSteering = ({ angle }) => {
   const canvasRef = useRef(null)
 
+  const [ctx, setCtx] = useState(null)
+  const [height, setHeight] = useState(0)
+  const [width, setWidth] = useState(0)
+
   useEffect(() => {
     const canvas = canvasRef.current
     const ctx = canvas.getContext('2d')
 
-    // Reset old line
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-    // Static setup
-    ctx.beginPath()
     ctx.strokeStyle = 'black'
     ctx.lineWidth = 10
+    setCtx(ctx)
+
+    setHeight(canvas.height)
+    setWidth(canvas.width)
+  }, [])
+
+  useEffect(() => {
+    if (!ctx) return // Not set on first invocation
+
+    // Reset old line
+    ctx.clearRect(0, 0, width, height)
 
     // Calculate line
-    const diff = (canvas.height / (18 * 2)) * angle
-    const middleY = canvas.height / 2
+    const diff = (height / (18 * 2)) * angle
+    const middleY = height / 2
     const y1 = middleY + diff
     const y2 = middleY - diff
 
     // Draw line
+    ctx.beginPath()
     ctx.moveTo(0, y1)
-    ctx.lineTo(canvas.width, y2)
+    ctx.lineTo(width, y2)
     ctx.stroke()
   }, [angle])
 
