90c73225f3b54fcc272d86af271c85f32f5e0d7f
Lorentz Lasson <lorentzlasson@users.noreply.github.com>
2019-10-10 07:16:33 +0200

    🔥 Backend (#859)

--- Procfile
+++
@@ -1,3 +0,0 @@
-# Workaround to please heroku
-# https://github.com/heroku/heroku-buildpack-nodejs/issues/385#issuecomment-291084067
-web: npm start --prefix backend
--- backend/src/config.js
+++
@@ -1,9 +0,0 @@
-const { env } = process
-
-const parseCsv = x => x.split(',')
-
-const config = {
-  corsWhitelist: env.CORS_WHITELIST ? parseCsv(env.CORS_WHITELIST) : [],
-}
-
-module.exports = config
